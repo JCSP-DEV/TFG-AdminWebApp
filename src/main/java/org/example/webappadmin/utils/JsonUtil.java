493d2d1e63c0fe004c1e2c1acb8e7f3dc065b01d
jc <23.dam.juancarlossanchez@fundacionmontessori.com>
2025-05-18 16:21:46 +0200

    Update project configuration to use JDK 21 for compatibility

--- src/main/java/org/example/webappadmin/utils/JsonUtil.java
+++ src/main/java/org/example/webappadmin/utils/JsonUtil.java
@@ -92,16 +92,3 @@ public class JsonUtil {
     }
 }
 
-// LocalDate adapter for Gson
-class LocalDateAdapter implements JsonSerializer<LocalDate>, JsonDeserializer<LocalDate> {
-    @Override
-    public JsonElement serialize(LocalDate src, Type typeOfSrc, JsonSerializationContext context) {
-        return new JsonPrimitive(src.toString());
-    }
-
-    @Override
-    public LocalDate deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) 
-            throws JsonParseException {
-        return LocalDate.parse(json.getAsString());
-    }
-} 
---
+++ src/main/java/org/example/webappadmin/utils/LocalDateAdapter.java
@@ -0,0 +1,20 @@
+package org.example.webappadmin.utils;
+
+import com.google.gson.*;
+
+import java.lang.reflect.Type;
+import java.time.LocalDate;
+
+// LocalDate adapter for Gson
+class LocalDateAdapter implements JsonSerializer<LocalDate>, JsonDeserializer<LocalDate> {
+    @Override
+    public JsonElement serialize(LocalDate src, Type typeOfSrc, JsonSerializationContext context) {
+        return new JsonPrimitive(src.toString());
+    }
+
+    @Override
+    public LocalDate deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+            throws JsonParseException {
+        return LocalDate.parse(json.getAsString());
+    }
+}
